f7a18e6c2d7ac618da1d5f3b293be1bc1a343e88
Gagandeep singh <g.deepsingh1@gmail.com>
2019-05-27 20:30:58 +0530

    hide content with ngIf

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -9,13 +9,13 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
         <div>Date: {{ event?.date }}</div>
         <div>Time: {{ event?.time }}</div>
         <div>Price: {{ event?.price | currency }}</div>
-        <div>
+        <div *ngIf="event?.location">
           <span>Location: {{ event?.location?.address }}</span>
           <span class="pad-left"
             >{{ event?.location?.city }}, {{ event?.location?.country }}</span
           >
         </div>
-        <div>online: {{ event?.onl }}</div>
+        <div *ngIf="event?.onl">online: {{ event?.onl }}</div>
       </div>
     </div>
   `,
