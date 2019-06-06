eb622cc84ce7585e0ce24870456a340bfa765409
Gagandeep singh <g.deepsingh1@gmail.com>
2019-06-06 20:10:30 +0530

    first template driven form

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Router, RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 import { ProfileComponent } from "./profile.component";
 import { USER_ROUTES } from "./user.routes";
+import { LoginComponent } from "./login.component";
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(USER_ROUTES)],
-  declarations: [ProfileComponent],
+  imports: [CommonModule, FormsModule, RouterModule.forChild(USER_ROUTES)],
+  declarations: [ProfileComponent, LoginComponent],
   providers: []
 })
 export class UserModule {}
