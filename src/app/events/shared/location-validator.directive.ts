d9f3d7703216c6198c4d88c3ba8fd8e4cd4c64f9
Gagandeep singh <g.deepsingh1@gmail.com>
2019-07-23 19:44:20 +0530

    Validator to angular validators

--- src/app/events/shared/location-validator.directive.ts
+++ src/app/events/shared/location-validator.directive.ts
@@ -1,8 +1,11 @@
 import { Directive } from '@angular/core';
-import { Validator, FormGroup } from '@angular/forms';
+import { Validator, FormGroup, NG_VALIDATORS } from '@angular/forms';
 
 @Directive({
-  selector: '[validateLocation]'
+  selector: '[validateLocation]',
+  providers: [
+    { provide: NG_VALIDATORS, useExisting: LocationValidate, multi: true }
+  ]
 })
 export class LocationValidate implements Validator {
   validate(formGroup: FormGroup) {
