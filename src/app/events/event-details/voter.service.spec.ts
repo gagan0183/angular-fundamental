783633c60893609af8fb942593a2f3efe45e6ed0
Gagandeep singh <g.deepsingh1@gmail.com>
2019-07-26 17:27:43 +0530

    testing mock calls

--- src/app/events/event-details/voter.service.spec.ts
+++ src/app/events/event-details/voter.service.spec.ts
@@ -19,5 +19,28 @@ describe('VoterService', () => {
       expect(session.voters.length).toBe(1);
       expect(session.voters[0]).toBe('john');
     });
+
+    it('should call http.delete with the right url', () => {
+      var session = { id: 9, voters: ['joe', 'john'] };
+      mockHttp.delete.and.returnValue(of(false));
+
+      voterService.deleteVote(9, <ISession>session, 'joe');
+      expect(mockHttp.delete).toHaveBeenCalledWith(
+        '/api/events/9/sessions/9/voters/joe'
+      );
+    });
+  });
+  describe('addVoter', () => {
+    it('should call http.post with the right url', () => {
+      var session = { id: 9, voters: ['john'] };
+      mockHttp.post.and.returnValue(of(false));
+
+      voterService.addVote(9, <ISession>session, 'joe');
+      expect(mockHttp.post).toHaveBeenCalledWith(
+        '/api/events/9/sessions/9/voters/joe',
+        {},
+        jasmine.any(Object)
+      );
+    });
   });
 });
