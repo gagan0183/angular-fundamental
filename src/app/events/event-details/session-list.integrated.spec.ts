1e9b6c4c59950670e6bb1dee0400fe04096cf000
Gagandeep singh <g.deepsingh1@gmail.com>
2019-07-27 10:29:48 +0530

    set up integrated test

---
+++ src/app/events/event-details/session-list.integrated.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed, async, ComponentFixture } from "@angular/core/testing";
+import { DebugElement } from "@angular/core";
+import { SessionListComponent } from "./session-list.component";
+import { AuthService } from "../../user/auth.service";
+import { VoterService } from "../shared/voter.service";
+import { ISession } from "../../user/user.model";
+import { By } from "@angular/platform-browser";
+
+describe("SessionListComponent", () => {
+  let fixture: ComponentFixture<SessionListComponent>;
+  let component: SessionListComponent;
+  let element: HTMLElement;
+  let debugEl: DebugElement;
+
+  beforeEach(async(() => {
+    let mockAuthService = {};
+    let mockVoterService = {};
+
+    TestBed.configureTestingModule({
+      imports: [],
+      declarations: [SessionListComponent],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: VoterService, useValue: mockVoterService }
+      ],
+      schemas: []
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SessionListComponent);
+    component = fixture.componentInstance;
+    debugEl = fixture.debugElement;
+    element = fixture.nativeElement;
+  });
+});
