6e40e63975fa58aa94b92e4b9d545e0c368e80ee
Gagandeep singh <g.deepsingh1@gmail.com>
2019-07-27 11:59:26 +0530

    integrate test

--- src/app/events/event-details/session-list.integrated.spec.ts
+++ src/app/events/event-details/session-list.integrated.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, async, ComponentFixture } from "@angular/core/testing";
-import { DebugElement } from "@angular/core";
+import { DebugElement, NO_ERRORS_SCHEMA } from "@angular/core";
 import { SessionListComponent } from "./session-list.component";
 import { AuthService } from "../../user/auth.service";
 import { VoterService } from "../shared/voter.service";
@@ -23,8 +23,8 @@ describe("SessionListComponent", () => {
         { provide: AuthService, useValue: mockAuthService },
         { provide: VoterService, useValue: mockVoterService }
       ],
-      schemas: []
-    });
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -32,5 +32,29 @@ describe("SessionListComponent", () => {
     component = fixture.componentInstance;
     debugEl = fixture.debugElement;
     element = fixture.nativeElement;
+  });
+
+  it("should have the correct session title", () => {
+    component.sessions = [
+      {
+        id: 1,
+        name: "session 1",
+        presenter: "p",
+        duration: 1,
+        level: "beginner",
+        abstract: "abstract",
+        voters: ["p"]
+      }
+    ];
+    component.filterBy = "all";
+    component.sortBy = "name";
+    component.eventId = 9;
+
+    component.ngOnChanges();
+    fixture.detectChanges();
+
+    expect(element.querySelector("[well-title]").textContent).toContain(
+      "session 1"
+    );
   });
 });
